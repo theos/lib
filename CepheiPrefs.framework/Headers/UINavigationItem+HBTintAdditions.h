71e9cc7e207cd2f87629a18e0c9362411d70cb6d
Adam Demasi <adam@hbang.com.au>
2016-03-31 22:55:34 +1030

    [global] Switch from binaries to plain text .tbd files.
    Additionally updates Cephei to the latest version (1.9).

--- CepheiPrefs.framework/Headers/UINavigationItem+HBTintAdditions.h
+++
@@ -1,8 +0,0 @@
-@interface UINavigationItem (HBTintAdditions)
-
-@property (nonatomic, copy) UIColor *hb_tintColor;
-@property (nonatomic, copy) UIColor *hb_navigationBarTintColor;
-@property (nonatomic, copy) UIColor *hb_navigationBarTextColor;
-@property (nonatomic, copy) UIColor *hb_navigationBarBackgroundColor;
-
-@end
