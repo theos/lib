1eef222ea55cc5fb3b52435465b06f9b7d6dfba9
kabiroberai <oberai.kabir@gmail.com>
2021-07-08 14:47:15 +0530

    [Orion] Clean up headers, update swiftinterface

--- Orion.framework/Headers/orion_public.h
+++ Orion.framework/Headers/orion_public.h
@@ -1,13 +1,9 @@
-#ifndef INIT_H_
-#define INIT_H_
+#ifndef ORION_PUBLIC_H
+#define ORION_PUBLIC_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-// this is only used in SPM mode but guarding it behind
-// a conditional confuses the compiler. Don't call it yourself.
-void _orion_init_c(void);
 
 // this symbol should always be found while linking; it's
 // declared in the Orion glue file as @_cdecl("orion_init")
